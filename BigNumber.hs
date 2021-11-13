d1cf9f5246189460ae388f514b63aa28279b2530
Marina <up201806787@fe.up.pt>
2021-11-13 16:51:07 +0000

    ready to start

---
+++ BigNumber.hs
@@ -0,0 +1,36 @@
+{--
+-- EXERCICIO 2
+-- Uma definição do tipo BigNumber
+BigNumber :: 
+
+--scanner, que converte uma string em big-number
+scanner :: String -> BigNumber
+scanner = 
+
+
+-- output, que converte um big-number em string
+output :: BigNumber -> String
+
+
+
+-- para somar dois big-numbers.
+somaBN ::  BigNumber -> BigNumber -> BigNumber
+
+
+
+-- subtrair dois big-numbers
+subBN ::  BigNumber -> BigNumber -> BigNumber
+
+
+--multiplicar dois big-numbers.
+mulBN ::  BigNumber -> BigNumber -> BigNumber
+
+
+--a divisão inteira de dois big-numbers. A divisão deverá retornar um par “(quociente, resto)”
+divBN :: BigNumber -> BigNumber -> (BigNumber,BigNumber)
+
+
+-- EXERCICIO 5
+safeDivBN ::
+safeDivBN =
+--}
---
+++ Fib.hs
@@ -0,0 +1,34 @@
+{--
+-- EXERCICIO 1
+
+--Uma função recursiva, fibRec
+fibRec :: (Integral a) => a -> a
+fibRec = 
+
+
+-- versão otimizada da função anterior, fibLista
+fibLista ::
+fibLista =
+
+--gerar uma lista infinita com todos os números de Fibonacci e retornar o elemento de ordem n,  fibListaInfinita
+fibListaInfinita ::
+fibListaInfinita =
+
+
+
+
+
+-- EXERCICIO 3
+-- Agora com BN
+fibRecBN :: 
+fibRecBN =
+
+
+fibListaBN ::
+fibListaBN = 
+
+
+fibListaInfinitaBN ::
+fibListaInfinitaBN =
+
+--}
