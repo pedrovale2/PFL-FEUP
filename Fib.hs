0f9bf5b6e5bd78a572f388b88926d0f000103c03
Marina <up201806787@fe.up.pt>
2021-11-13 18:04:44 +0000

    exercice 1 done

--- Fib.hs
+++ Fib.hs
@@ -1,22 +1,23 @@
-{--
--- EXERCICIO 1
-
 --Uma função recursiva, fibRec
 fibRec :: (Integral a) => a -> a
-fibRec = 
+fibRec 0 = 0
+fibRec 1 = 1
+fibRec n = fibRec (n-1) + fibRec (n-2)
 
 
 -- versão otimizada da função anterior, fibLista
-fibLista ::
-fibLista =
+--fibLista ::
+--fibLista :: Int -> Int
+fibLista :: Integral a => Int -> [a] -> a
+fibLista i lista = head(drop i [fibRec x|x<-lista])
+
 
 --gerar uma lista infinita com todos os números de Fibonacci e retornar o elemento de ordem n,  fibListaInfinita
-fibListaInfinita ::
-fibListaInfinita =
+fibListaInfinita :: Integral a => Int -> a
+fibListaInfinita i = head(drop i [fibRec x|x<-[0..]])
 
 
-
-
+{--
 
 -- EXERCICIO 3
 -- Agora com BN
